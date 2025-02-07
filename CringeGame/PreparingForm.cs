26138b9a05806718d64dde7b1356dd069cef9352
AveryanovKirill <kirill.kfu@gmail.com>
2025-02-07 17:59:26 +0300

    [update] PreparingForm.cs

--- CringeGame/PreparingForm.cs
+++ CringeGame/PreparingForm.cs
@@ -27,11 +27,11 @@ namespace CringeGame
 
         private void PreparingForm_Load(object sender, EventArgs e)
         {
-            var player = new Player("Салимов");
-            var player2 = new Player("Тимерхан");
-            var player3 = new Player("Алмаз");
-            var player4 = new Player("Цивилийский");
-            var player5 = new Player("Погогите :(");
+            var player = new Player("Игрок 1");
+            var player2 = new Player("Игрок 2");
+            var player3 = new Player("Игрок 3");
+            var player4 = new Player("Игрок 4");
+            var player5 = new Player("Игрок 5");
             _players.Add(player);
             _players.Add(player2);
             _players.Add(player3);
@@ -49,25 +49,6 @@ namespace CringeGame
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //var player = new Player("Салимов"); // подтягивать имя с другой формы
-            //var player2 = new Player("Тимерхан");
-            //var player3 = new Player("Алмаз");
-            //var player4 = new Player("Цивилийский");
-            //var player5 = new Player("Погогите :(");
-            //_players.Add(player);
-            //_players.Add(player2);
-            //_players.Add(player3);
-            //_players.Add(player4);
-            //_players.Add(player5);
-            //_currentPlayer = player;
-            //if (TryGetAllPlayers())
-            //{
-            //    var game = new Game(_players, _currentPlayer);
-            //    mainForm.SetGame(game);
-            //    mainForm.PanelForm(new ChooseRoleForm(mainForm));
-            //    //ChooseForm();
-            //}
-
             var game = new Game(_players, _currentPlayer);
             mainForm.SetGame(game);
             mainForm.PanelForm(new ChooseRoleForm(mainForm));
